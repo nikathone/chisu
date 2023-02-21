49a7976d683676f686a0299ef71c72f5c3c48419
Daniel Cothran <daniel_cothran@jsi.com>
2023-02-21 15:23:00 -0600

    Update the global.js file for the search button functionality

--- js/global.js
+++ js/global.js
@@ -9,7 +9,13 @@
 
   Drupal.behaviors.chisu = {
     attach: function (context, settings) {
-
+      let myButton = jQuery('#search-expand');
+      myButton.on('click', function () {
+        jQuery('#block-exposedformsearchpage-1').toggleClass('d-block');
+        myButton.toggleClass('search-block-button-expanded');
+        jQuery('input#edit-keys').focus();
+        console.log('clicked');
+      });
     }
   };
 
